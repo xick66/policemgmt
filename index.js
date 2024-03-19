e1f958c3b0b6dddfe25910818c897c23a358fe27
xick66 <rahulkm086@gmail.com>
2024-03-20 00:35:50 +0530

    pushhh

--- index.js
+++ index.js
@@ -24,6 +24,28 @@ app.get('/',(req,res)=>{
     res.send("hello I am up")
 })
 
+app.get('/fetch-data', async (req, res) => {
+    try {
+      const url = 'https://ef6c-34-136-255-126.ngrok-free.app/data';
+      
+      // Add headers to bypass ngrok's warning page
+      const config = {
+        headers: {
+          'ngrok-skip-browser-warning': 'anyvalue', // You can set this header to any value
+          'User-Agent': 'MyCustomUserAgent' // Alternatively, set a custom User-Agent
+        }
+      };
+      
+      // Use axios to send a GET request to the URL with additional headers
+      const response = await axios.get(url, config);
+      
+      // Send the response data back to the client
+      res.send(response.data);
+    } catch (error) {
+      res.status(500).send({ error: 'Failed to fetch data' });
+    }
+  });
+
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
